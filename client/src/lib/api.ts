92596f3ac869945a95a8663af5d5501b0ddf4298
Swati Chaudhary <swati1623.be23@chitkarauniversity.edu.in>
2026-05-03 22:45:31 +0530

    fixed api url

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,4 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5002";
+//const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5002";
+const API_URL = import.meta.env.VITE_API_URL!;
 
 export class ApiError extends Error {
   status: number;
